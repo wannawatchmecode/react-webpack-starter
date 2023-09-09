dc3ffa73fa757f1593010a42d556c196849a24f8
unknown <wannawatchmecode@gmail.com>
2023-09-09 15:29:12 -0400

    Initial commit of template
    Add template using webpack, react, and typescript.
    
    Testing: npm run build successfull

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, "/assets"), // the bundle output path
+    filename: "bundle.js", // the name of the bundle
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html", // to import index.html file inside index.js
+    }),
+  ],
+  devServer: {
+    port: 3030, // you can change the port
+  },
+  module: {
+    rules: [
+        {
+            test: /\.tsx?$/,
+            exclude: /node_modules/,
+            use: [{
+              loader: 'ts-loader',
+            options: {
+                configFile: "tsconfig.webpack.json"
+              },
+            },
+          ],
+        },
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // styles files
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+        loader: "url-loader",
+        options: { limit: false },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
